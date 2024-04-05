c74cf1c8b0089687346f1ea39420deceb34c13cf
Ayush Sharma <ayushsharma3310@gmail.com>
2024-04-05 11:36:00 +0530

    client@server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studynotion-six-pied.vercel.app",
+		origin: process.env.CLIENT,
 		credentials:true,
 	})
 )
