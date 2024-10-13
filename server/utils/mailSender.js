45211f5c4b76a3b0bec01feb8ca8267c7a0d733d
Ayush Sharma <ayushsharma3310@gmail.com>
2024-10-13 19:04:26 +0530

    update ReadMe.md

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -17,7 +17,7 @@ const mailSender = async (email, title, body) => {
             })
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+                from: 'StudyNotion || by Ayush',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
